beea61a9124450c64ad7199b15d57ff26752e2fb
ju1ius <jules.bernable@laposte.net>
2014-07-16 09:08:15 +0200

    simplified format string parsing

--- clisnips/strfmt/fmt_parser.py
+++ clisnips/strfmt/fmt_parser.py
@@ -1,63 +1,147 @@
+import re
 from string import Formatter
 
 from ..exceptions import ParsingError
-from .doc_tokens import * 
 
 
+FIELD_NAME_RX = re.compile(r'''
+^
+(\d+|[_a-zA-Z]\w*)      # arg_name
+(?:
+    \.[_a-zA-Z]\w*      # getattr 
+    |
+    \[ \d+ | [^\]]+ \]  # getitem
+)*
+$
+''', re.X)
 __lexer = Formatter()
 
 
-def lex(fmt_str):
-    stream = __lexer.parse(fmt_str)
-    pos = line = col = 0
+def parse(text):
+    try:
+        stream = __lexer.parse(text)
+    except Exception as err:
+        raise ParsingError(str(err))
+    fields = []
+    auto_count = -1
+    has_explicit_numeric_field = False
     for prefix, param, spec, conv in stream:
-        if prefix:
-            t = Token(T_TEXT, line, col, prefix)
-            t.startpos = pos
-            l = len(prefix)
-            pos += l
-            last_line_pos = prefix.rfind('\n')
-            if last_line_pos != -1:
-                line += prefix.count('\n')
-                col = l - last_line_pos - 1
-            else:
-                col += l
-            t.endline, t.endcol, t.endpos = line, col, pos
-            yield t
         if param is None:
             continue
+        name = ''
+        if param:
+            m = FIELD_NAME_RX.match(param)
+            if not m:
+                raise ParsingError('Invalid replacement field %r' % param)
+            name = m.group(1)
+        if not name:
+            if has_explicit_numeric_field:
+                raise ParsingError('Cannot switch from manual '
+                                   'to automatic field numbering')
+            auto_count += 1
+            name = auto_count
+        elif name.isdigit():
+            has_explicit_numeric_field = True
+            if auto_count > -1:
+                raise ParsingError('Cannot switch from automatic '
+                                   'to manual field numbering')
+        fields.append({
+            'name': name,
+            'identifier': param,
+            'spec': spec,
+            'conv': conv
+        })
+    return fields
+
+
+# Begin Deprecated code
+
+from .doc_tokens import *
+
+
+PARAM_NAME_RX = re.compile(r'\d+|[_a-zA-Z]\w*')
+
+
+class Lexer(object):
+
+    def __init__(self, text):
+        self.text = text
+
+    def __iter__(self):
+        stream = __lexer.parse(self.text)
+        self.pos = 0
+        self.line = self.col = 1
+        for prefix, param, spec, conv in stream:
+            if prefix:
+                yield self._handle_text(prefix)
+            if param is None:
+                continue
+            yield self._handle_field(param, spec, conv)
+
+    def _handle_text(self, text):
+        t = self.init_token(T_TEXT, text)
+        l = len(text)
+        self.pos += l
+        last_line_pos = text.rfind('\n')
+        if last_line_pos != -1:
+            self.line += text.count('\n')
+            self.col = l - last_line_pos - 1
+        else:
+            self.col += l
+        return self.finalize_token(t)
+
+    def _handle_field(self, param, spec, conv):
         #FIXME:
         # the docstring parser doesn't support item/attribute access,
         # or exotic characters (e.g. linebreaks) in identifiers.
         # For now we just raise an exception.
-        value = {'identifier': param, 'spec': spec, 'conv': conv}
-        t = Token(T_PARAM, line, col, value)
-        t.startpos = pos
+        name = ''
+        if param:
+            m = PARAM_NAME_RX.match(param)
+            name = m.group(0)
+        value = {
+            'name': name,
+            'identifier': param,
+            'spec': spec,
+            'conv': conv
+        }
+        t = self.init_token(T_PARAM, value)
         l = len(param) + 2  # account for the "{}" delimiters
         if spec:
             l += len(spec) + 1  # account for the ":" separator
         if conv:
             l += len(conv) + 1  # account for the "!" separator
-        pos += l
-        col += l
-        t.endline, t.endcol, t.endpos = line, col, pos
-        yield t
+        self.pos += l
+        self.col += l
+        return self.finalize_token(t)
+
+    def init_token(self, type, value=None):
+        token = Token(type, self.line, self.col, value)
+        token.startpos = self.pos
+        return token
+
+    def finalize_token(self, token, line=None, col=None, pos=None, value=None):
+        if value is not None:
+            token.value = value
+        token.endpos = pos if pos is not None else self.pos
+        token.endline = line if line is not None else self.line
+        token.endcol = col if col is not None else self.col
+        return token
 
 
-def parse(fmt_str):
+def __parse(fmt_str):
     auto_count = -1
     has_numeric_field = False
-    for token in lex(fmt_str):
+    fields = []
+    for token in Lexer(fmt_str):
         if token.type == T_PARAM:
-            name = token.value['identifier']
-            if name == '':
+            name = token.value['name']
+            if not name:
                 if has_numeric_field:
-                    raise ParsingError(
-                        'Cannot switch from manual '
-                        'to automatic field numbering'
-                    )
+                    raise ParsingError('Cannot switch from manual '
+                                       'to automatic field numbering')
                 auto_count += 1
-                token.value['identifier'] = auto_count
+                token.value['name'] = auto_count
             else:
                 try:
                     name = int(name)
@@ -65,17 +149,7 @@ def parse(fmt_str):
                 except ValueError:
                     is_numeric = False
                 if is_numeric and auto_count > -1:
-                    raise ParsingError(
-                        'Cannot switch from automatic '
-                        'to manual field numbering'
-                    )
-        yield token
-
-
-if __name__ == '__main__':
-    import sys 
-    fmt_str = 'foo: {}\nbar: {w00t}\nblah: {yay}'
-    if len(sys.argv) > 1:
-        fmt_str = sys.argv[1]
-    for token in parse(fmt_str):
-        print token
+                    raise ParsingError('Cannot switch from automatic '
+                                       'to manual field numbering')
+            fields.append(token.value)
+    return fields
