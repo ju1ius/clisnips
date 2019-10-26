af2792471933c674752fd16aeaa39cc11e3aa8b0
ju1ius <jules.bernable@gmail.com>
2019-10-26 17:47:05 +0200

    improves PathEntry autocompletion

--- clisnips/gui/strfmt_widgets.py
+++ clisnips/gui/strfmt_widgets.py
@@ -1,9 +1,8 @@
-import locale
-import os
 from pathlib import Path
 
-from gi.repository import GLib, GObject, Gio, Gtk
+from gi.repository import GObject, Gtk
 
+from .path_entry import PathEntry as _PathEntry
 from ..strfmt.doc_nodes import ValueList, ValueRange
 from ..utils.number import get_num_decimals
 
@@ -142,176 +141,49 @@ class PathEntry(Gtk.Box):
         'changed': (GObject.SignalFlags.RUN_LAST, None, ())
     }
 
-    COLUMN_ICON, COLUMN_TEXT = range(2)
-    COLUMNS = (Gio.Icon, str)
-    COMPLETION_TIMEOUT = 300
-
     MODE_PATH = 'path'
     MODE_FILE = 'file'
     MODE_DIR = 'dir'
 
     def __init__(self, cwd=None, mode=None, default=''):
         super().__init__(spacing=6)
-        self.set_orientation(Gtk.Orientation.VERTICAL)
-        self._filechooser = None
+        self.set_orientation(Gtk.Orientation.HORIZONTAL)
+        self._file_chooser = None
         self._cwd: Path = Path(cwd) if cwd is not None else Path.cwd()
         self._mode = mode or self.MODE_FILE
         self.show_files = self._mode in (self.MODE_PATH, self.MODE_FILE)
         self._completion_timeout = 0
 
         # setup text entry
-        self._entry = Gtk.Entry()
+        self._entry = _PathEntry()
         if default:
             self._entry.set_text(default)
         self.pack_start(self._entry, expand=True, fill=True, padding=0)
-        self._entry.connect("changed", self._on_entry_changed)
 
         # setup filechooser
         if self._mode in (self.MODE_FILE, self.MODE_DIR):
-            self._filechooser = Gtk.FileChooserButton('')
+            self._file_chooser = Gtk.FileChooserButton('')
             if self._mode == self.MODE_FILE:
                 title = 'Select a file'
                 action = Gtk.FileChooserAction.OPEN
             else:
                 title = 'Select a folder'
                 action = Gtk.FileChooserAction.SELECT_FOLDER
-            self._filechooser.set_title(title)
-            self._filechooser.set_action(action)
-            self._filechooser.set_current_folder(str(self._cwd))
-            self._filechooser.connect('file-set', self._on_filechooser_file_set)
-            self.pack_start(self._filechooser, expand=False, fill=True, padding=0)
-
-        # setup completion
-        self._completion = PathCompletion(self._cwd, self.show_files)
-        self._entry.set_completion(self._completion)
+            self._file_chooser.set_title(title)
+            self._file_chooser.set_action(action)
+            self._file_chooser.set_current_folder(str(self._cwd))
+            self._file_chooser.connect('file-set', self._on_file_chooser_file_set)
+            self.pack_start(self._file_chooser, expand=False, fill=True, padding=0)
 
     def get_value(self):
         return self._entry.get_text()
 
     def set_cwd(self, cwd=None):
-        self._cwd = Path(cwd) if cwd is not None else Path.cwd()
-        self._completion.set_cwd(self._cwd)
-        if self._filechooser:
-            self._filechooser.set_current_folder(str(self._cwd))
+        self._entry.set_working_directory(cwd)
+        if self._file_chooser:
+            self._file_chooser.set_current_folder(str(self._cwd))
 
-    def _on_entry_changed(self, entry):
-        if self._completion_timeout:
-            GLib.source_remove(self._completion_timeout)
-        self._completion_timeout = GLib.timeout_add(
-            self.COMPLETION_TIMEOUT,
-            self._on_completion_timeout
-        )
-
-    def _on_filechooser_file_set(self, btn):
-        selected = self._filechooser.get_filename()
+    def _on_file_chooser_file_set(self, btn):
+        selected = self._file_chooser.get_filename()
         if selected:
             self._entry.set_text(selected)
-
-    def _on_completion_timeout(self):
-        self._completion_timeout = 0
-        self.emit('changed')
-
-        text = self._entry.get_text()
-        if text:
-            self._completion.get_matches(text)
-        return False
-
-    def _get_completions(self, text):
-        path = Path(text).expanduser()
-        if not path.is_absolute():
-            path = self._cwd / path
-        if not path.parent.exists():
-            return
-        for fp in path.parent.glob(f'{path.name}*'):
-            if fp.is_dir():
-                yield fp, 'inode/directory'
-            elif self.show_files and fp.is_file():
-                ft, uncertain = Gio.content_type_guess(str(fp))
-                yield fp, ft
-        # yield from self._glob(path.parent, f'{path.name}*')
-
-    def _glob(self, path: Path, pattern: str):
-        dirs, files = [], []
-        for fp in path.glob(pattern):
-            if fp.is_dir():
-                dirs.append((fp, 'inode/directory'))
-            elif self.show_files and fp.is_file():
-                ft, uncertain = Gio.content_type_guess(str(fp))
-                files.append((fp, ft))
-        dirs = sorted(dirs, key=self._sort)
-        print(dirs, files)
-        if self.show_files:
-            yield from iter(dirs + sorted(files, key=self._sort))
-        yield from iter(dirs)
-
-    def _sort(self, item):
-        return locale.strxfrm(str(item[0]))
-
-
-class PathCompletion(Gtk.EntryCompletion):
-
-    COLUMN_ICON, COLUMN_NAME, COLUMN_PATH = range(3)
-    COLUMNS = (Gio.Icon, str, str)
-
-    def __init__(self, cwd, show_files=True):
-        super().__init__()
-        self._cwd = cwd
-        self._show_files = show_files
-        self.set_model(Gtk.ListStore(*self.COLUMNS))
-        self.set_match_func(self._match_func, {'foo': 'bar'})
-        pixbuf_cell = Gtk.CellRendererPixbuf()
-        self.pack_start(pixbuf_cell, expand=False)
-        self.add_attribute(pixbuf_cell, 'gicon', self.COLUMN_ICON)
-        text_cell = Gtk.CellRendererText()
-        self.pack_start(text_cell, expand=False)
-        self.add_attribute(text_cell, 'text', self.COLUMN_NAME)
-        # self.set_text_column(self.COLUMN_NAME)
-        # self.connect('match-selected', self._on_match_selected)
-
-    def set_cwd(self, cwd):
-        self._cwd = Path(cwd) if cwd is not None else Path.cwd()
-
-    def get_matches(self, text: str):
-        model = self.get_model()
-        model.clear()
-        for name, path, mime_type in self._get_completions(text):
-            model.append((
-                Gio.content_type_get_icon(mime_type),
-                name,
-                path
-            ))
-
-    def _get_completions(self, text):
-        path = os.path.expanduser(text)
-        if not os.path.isabs(path):
-            path = os.path.join(self._cwd, path)
-        dirname, basename = os.path.split(path)
-        if not os.path.exists(dirname):
-            return
-        if path.endswith('/') and os.path.isdir(path):
-            entries = os.scandir(path)
-        else:
-            entries = (f for f in os.scandir(dirname) if f.name.lower().startswith(basename.lower()))
-        for entry in sorted(entries, key=self._sort_entries):
-            if entry.is_dir():
-                yield entry.name, entry.path, 'inode/directory'
-            elif self._show_files and entry.is_file():
-                ft, uncertain = Gio.content_type_guess(entry.path)
-                yield entry.name, entry.path, ft
-
-    @staticmethod
-    def _match_func(completion, key, tree_iter, data=None):
-        # print(f'Match func: {key} ({self.get_entry().get_text()})')
-        return True
-
-    def do_match_selected(self, model, tree_iter):
-        path = model.get_value(tree_iter, self.COLUMN_PATH)
-        entry: Gtk.Entry = self.get_entry()
-        entry.set_text(path)
-        entry.set_position(entry.get_text_length())
-
-        return True
-
-    @staticmethod
-    def _sort_entries(entry):
-        return locale.strxfrm(entry.name)
---
+++ clisnips/utils/common_prefix.py
@@ -0,0 +1,11 @@
+
+
+def common_prefix(*args: str) -> str:
+    s1 = min(args)
+    s2 = max(args)
+    common = s1
+    for i, char in enumerate(s1):
+        if char != s2[i]:
+            common = s1[:i]
+            break
+    return common
