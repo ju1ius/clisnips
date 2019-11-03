b764588c68aa119ea48022680138d65d7728e568
ju1ius <jules.bernable@gmail.com>
2019-11-03 18:11:26 +0100

    converts unit tests to pytest

--- tests/strfmt/doc_lexer_test.py
+++ tests/strfmt/doc_lexer_test.py
@@ -1,265 +1,267 @@
-import unittest
-
-from clisnips.strfmt.doc_tokens import *
 from clisnips.strfmt.doc_lexer import Lexer
+from clisnips.strfmt.doc_tokens import *
 
 
-class DocLexerTest(unittest.TestCase):
+def assert_token_list_equal(actual_tokens, expected_tokens):
+    assert len(actual_tokens) == len(expected_tokens)
+    for i, exp in enumerate(expected_tokens):
+        token = actual_tokens[i]
+        for attr in exp.keys():
+            actual = getattr(token, attr)
+            expected = exp[attr]
+            assert expected == actual, 'Expected token.{} to equal {!r}, got {!r}'.format(
+                attr,
+                token_name(expected) if attr == 'type' else expected,
+                token_name(actual) if attr == 'type' else actual
+            )
 
-    def assertTokenListEqual(self, actual_tokens, expected_tokens):
-        self.assertEqual(len(actual_tokens), len(expected_tokens))
-        for i, exp in enumerate(expected_tokens):
-            token = actual_tokens[i]
-            for attr in exp.keys():
-                actual = getattr(token, attr)
-                expected = exp[attr]
-                self.assertEqual(
-                    actual,
-                    expected,
-                    'Expected token.{} to equal {!r}, got {!r}'.format(
-                        attr,
-                        token_name(expected) if attr == 'type' else expected,
-                        token_name(actual) if attr == 'type' else actual
-                    )
-                )
 
-    def _tokenize(self, text):
-        lexer = Lexer(text)
-        return [t for t in Lexer(text)]
+def tokenize(text):
+    return list(Lexer(text))
 
-    def testFreeTextOnly(self):
-        text = """
-            This is the global description of the command.
-            It's all text until a {parameter} is seen.
-            A {param} must start a line (possibly indented).
-        """
-        lexer = iter(Lexer(text))
-        token = next(lexer)
-        self.assertEqual(token.type, T_TEXT)
-        self.assertEqual(token.value, text)
 
-    def testParamOnly(self):
-        text = '{par1}\n{par2}'
-        tokens = [t for t in Lexer(text)]
-        expected = [
-            {'type': T_LBRACE},
-            {'type': T_IDENTIFIER, 'value': 'par1'},
-            {'type': T_RBRACE},
-            {'type': T_TEXT, 'value': '\n'},
-            {'type': T_LBRACE},
-            {'type': T_IDENTIFIER, 'value': 'par2'},
-            {'type': T_RBRACE},
-            {'type': T_EOF}
-        ]
-        self.assertTokenListEqual(tokens, expected)
+def test_free_text_only():
+    text = """
+        This is the global description of the command.
+        It's all text until a {parameter} is seen.
+        A {param} must start a line (possibly indented).
+    """
+    lexer = iter(Lexer(text))
+    token = next(lexer)
+    assert token.type == T_TEXT
+    assert token.value == text
 
-    def testFlags(self):
-        text = '{-h}\n{--some-flag}'
-        tokens = [t for t in Lexer(text)]
-        expected = [
-            {'type': T_LBRACE},
-            {'type': T_FLAG, 'value': '-h'},
-            {'type': T_RBRACE},
-            {'type': T_TEXT, 'value': '\n'},
-            {'type': T_LBRACE},
-            {'type': T_FLAG, 'value': '--some-flag'},
-            {'type': T_RBRACE},
-            {'type': T_EOF}
-        ]
-        self.assertTokenListEqual(tokens, expected)
 
-    def testTypeHintOnly(self):
-        text = '{par1} ( string )'
-        tokens = [t for t in Lexer(text)]
-        expected = [
-            {'type': T_LBRACE},
-            {'type': T_IDENTIFIER, 'value': 'par1'},
-            {'type': T_RBRACE},
-            {'type': T_LPAREN},
-            {'type': T_IDENTIFIER, 'value': 'string'},
-            {'type': T_RPAREN},
-            {'type': T_EOF}
-        ]
-        self.assertTokenListEqual(tokens, expected)
-        #FIXME: test that invalid typehint is skipped
+def test_param_only():
+    text = '{par1}\n{par2}'
+    tokens = tokenize(text)
+    expected = [
+        {'type': T_LBRACE},
+        {'type': T_IDENTIFIER, 'value': 'par1'},
+        {'type': T_RBRACE},
+        {'type': T_TEXT, 'value': '\n'},
+        {'type': T_LBRACE},
+        {'type': T_IDENTIFIER, 'value': 'par2'},
+        {'type': T_RBRACE},
+        {'type': T_EOF}
+    ]
+    assert_token_list_equal(tokens, expected)
 
-    def testValueHintStringList(self):
-        # string list
-        text = '{par1} ["value1", *"value2"]'
-        tokens = [t for t in Lexer(text)]
-        expected = [
-            {'type': T_LBRACE},
-            {'type': T_IDENTIFIER, 'value': 'par1'},
-            {'type': T_RBRACE},
-            {'type': T_LBRACK},
-            {'type': T_STRING, 'value': 'value1'},
-            {'type': T_COMMA},
-            {'type': T_STAR},
-            {'type': T_STRING, 'value': 'value2'},
-            {'type': T_RBRACK},
-            {'type': T_EOF}
-        ]
-        self.assertTokenListEqual(tokens, expected)
 
-    def testValueHintDigitList(self):
-        # digit list
-        text = '{par1} [1, *-2, 0.3]'
-        tokens = [t for t in Lexer(text)]
-        expected = [
-            {'type': T_LBRACE},
-            {'type': T_IDENTIFIER, 'value': 'par1'},
-            {'type': T_RBRACE},
-            {'type': T_LBRACK},
-            {'type': T_INTEGER, 'value': '1'},
-            {'type': T_COMMA},
-            {'type': T_STAR},
-            {'type': T_INTEGER, 'value': '-2'},
-            {'type': T_COMMA},
-            {'type': T_FLOAT, 'value': '0.3'},
-            {'type': T_RBRACK},
-            {'type': T_EOF}
-        ]
-        self.assertTokenListEqual(tokens, expected)
+def test_flags():
+    text = '{-h}\n{--some-flag}'
+    tokens = tokenize(text)
+    expected = [
+        {'type': T_LBRACE},
+        {'type': T_FLAG, 'value': '-h'},
+        {'type': T_RBRACE},
+        {'type': T_TEXT, 'value': '\n'},
+        {'type': T_LBRACE},
+        {'type': T_FLAG, 'value': '--some-flag'},
+        {'type': T_RBRACE},
+        {'type': T_EOF}
+    ]
+    assert_token_list_equal(tokens, expected)
 
-    def testValueHintRange(self):
-        # simple range
-        text = '{par1} [1:5]'
-        tokens = [t for t in Lexer(text)]
-        expected = [
-            {'type': T_LBRACE},
-            {'type': T_IDENTIFIER, 'value': 'par1'},
-            {'type': T_RBRACE},
-            {'type': T_LBRACK},
-            {'type': T_INTEGER, 'value': '1'},
-            {'type': T_COLON},
-            {'type': T_INTEGER, 'value': '5'},
-            {'type': T_RBRACK},
-            {'type': T_EOF}
-        ]
-        self.assertTokenListEqual(tokens, expected)
-        # range with step
-        text = '{par1} [1:10:2]'
-        tokens = [t for t in Lexer(text)]
-        expected = [
-            {'type': T_LBRACE},
-            {'type': T_IDENTIFIER, 'value': 'par1'},
-            {'type': T_RBRACE},
-            {'type': T_LBRACK},
-            {'type': T_INTEGER, 'value': '1'},
-            {'type': T_COLON},
-            {'type': T_INTEGER, 'value': '10'},
-            {'type': T_COLON},
-            {'type': T_INTEGER, 'value': '2'},
-            {'type': T_RBRACK},
-            {'type': T_EOF}
-        ]
-        self.assertTokenListEqual(tokens, expected)
-        # range with step and default
-        text = '{ par1 } [1 : 10 : 2 * 5]'
-        tokens = [t for t in Lexer(text)]
-        expected = [
-            {'type': T_LBRACE},
-            {'type': T_IDENTIFIER, 'value': 'par1'},
-            {'type': T_RBRACE},
-            {'type': T_LBRACK},
-            {'type': T_INTEGER, 'value': '1'},
-            {'type': T_COLON},
-            {'type': T_INTEGER, 'value': '10'},
-            {'type': T_COLON},
-            {'type': T_INTEGER, 'value': '2'},
-            {'type': T_STAR},
-            {'type': T_INTEGER, 'value': '5'},
-            {'type': T_RBRACK},
-            {'type': T_EOF}
-        ]
-        self.assertTokenListEqual(tokens, expected)
 
-    def testFreeTextAfterParam(self):
-        text = '{ par1 } Some free text (wow, [snafu])'
-        tokens = [t for t in Lexer(text)]
-        expected = [
-            {'type': T_LBRACE},
-            {'type': T_IDENTIFIER, 'value': 'par1'},
-            {'type': T_RBRACE},
-            {'type': T_TEXT},
-            {'type': T_EOF}
-        ]
-        self.assertTokenListEqual(tokens, expected)
+def test_type_hint_only():
+    text = '{par1} ( string )'
+    tokens = tokenize(text)
+    expected = [
+        {'type': T_LBRACE},
+        {'type': T_IDENTIFIER, 'value': 'par1'},
+        {'type': T_RBRACE},
+        {'type': T_LPAREN},
+        {'type': T_IDENTIFIER, 'value': 'string'},
+        {'type': T_RPAREN},
+        {'type': T_EOF}
+    ]
+    assert_token_list_equal(tokens, expected)
+    # FIXME: test that invalid typehint is skipped
 
-        text = '''{ par1 } Some free text (wow, [snafu])
-            {par2} (hint) Other text
-            {par3} (hint) ["foo"] Yet another doctext
-        '''
-        tokens = [t for t in Lexer(text)]
-        expected = [
-            {'type': T_LBRACE},
-            {'type': T_IDENTIFIER, 'value': 'par1'},
-            {'type': T_RBRACE},
-            {'type': T_TEXT},
-            {'type': T_LBRACE},
-            {'type': T_IDENTIFIER, 'value': 'par2'},
-            {'type': T_RBRACE},
-            {'type': T_LPAREN},
-            {'type': T_IDENTIFIER, 'value': 'hint'},
-            {'type': T_RPAREN},
-            {'type': T_TEXT},
-            {'type': T_LBRACE},
-            {'type': T_IDENTIFIER, 'value': 'par3'},
-            {'type': T_RBRACE},
-            {'type': T_LPAREN},
-            {'type': T_IDENTIFIER, 'value': 'hint'},
-            {'type': T_RPAREN},
-            {'type': T_LBRACK},
-            {'type': T_STRING, 'value': 'foo'},
-            {'type': T_RBRACK},
-            {'type': T_TEXT},
-            {'type': T_EOF}
-        ]
-        self.assertTokenListEqual(tokens, expected)
 
-    def testCodeBlock(self):
-        text = '''
+def test_value_hint_string_list():
+    # string list
+    text = '{par1} ["value1", *"value2"]'
+    tokens = tokenize(text)
+    expected = [
+        {'type': T_LBRACE},
+        {'type': T_IDENTIFIER, 'value': 'par1'},
+        {'type': T_RBRACE},
+        {'type': T_LBRACK},
+        {'type': T_STRING, 'value': 'value1'},
+        {'type': T_COMMA},
+        {'type': T_STAR},
+        {'type': T_STRING, 'value': 'value2'},
+        {'type': T_RBRACK},
+        {'type': T_EOF}
+    ]
+    assert_token_list_equal(tokens, expected)
+
+
+def test_value_hint_digit_list():
+    # digit list
+    text = '{par1} [1, *-2, 0.3]'
+    tokens = tokenize(text)
+    expected = [
+        {'type': T_LBRACE},
+        {'type': T_IDENTIFIER, 'value': 'par1'},
+        {'type': T_RBRACE},
+        {'type': T_LBRACK},
+        {'type': T_INTEGER, 'value': '1'},
+        {'type': T_COMMA},
+        {'type': T_STAR},
+        {'type': T_INTEGER, 'value': '-2'},
+        {'type': T_COMMA},
+        {'type': T_FLOAT, 'value': '0.3'},
+        {'type': T_RBRACK},
+        {'type': T_EOF}
+    ]
+    assert_token_list_equal(tokens, expected)
+
+
+def test_value_hint_range():
+    # simple range
+    text = '{par1} [1:5]'
+    tokens = tokenize(text)
+    expected = [
+        {'type': T_LBRACE},
+        {'type': T_IDENTIFIER, 'value': 'par1'},
+        {'type': T_RBRACE},
+        {'type': T_LBRACK},
+        {'type': T_INTEGER, 'value': '1'},
+        {'type': T_COLON},
+        {'type': T_INTEGER, 'value': '5'},
+        {'type': T_RBRACK},
+        {'type': T_EOF}
+    ]
+    assert_token_list_equal(tokens, expected)
+    # range with step
+    text = '{par1} [1:10:2]'
+    tokens = tokenize(text)
+    expected = [
+        {'type': T_LBRACE},
+        {'type': T_IDENTIFIER, 'value': 'par1'},
+        {'type': T_RBRACE},
+        {'type': T_LBRACK},
+        {'type': T_INTEGER, 'value': '1'},
+        {'type': T_COLON},
+        {'type': T_INTEGER, 'value': '10'},
+        {'type': T_COLON},
+        {'type': T_INTEGER, 'value': '2'},
+        {'type': T_RBRACK},
+        {'type': T_EOF}
+    ]
+    assert_token_list_equal(tokens, expected)
+    # range with step and default
+    text = '{ par1 } [1 : 10 : 2 * 5]'
+    tokens = tokenize(text)
+    expected = [
+        {'type': T_LBRACE},
+        {'type': T_IDENTIFIER, 'value': 'par1'},
+        {'type': T_RBRACE},
+        {'type': T_LBRACK},
+        {'type': T_INTEGER, 'value': '1'},
+        {'type': T_COLON},
+        {'type': T_INTEGER, 'value': '10'},
+        {'type': T_COLON},
+        {'type': T_INTEGER, 'value': '2'},
+        {'type': T_STAR},
+        {'type': T_INTEGER, 'value': '5'},
+        {'type': T_RBRACK},
+        {'type': T_EOF}
+    ]
+    assert_token_list_equal(tokens, expected)
+
+
+def test_free_text_after_param():
+    text = '{ par1 } Some free text (wow, [snafu])'
+    tokens = tokenize(text)
+    expected = [
+        {'type': T_LBRACE},
+        {'type': T_IDENTIFIER, 'value': 'par1'},
+        {'type': T_RBRACE},
+        {'type': T_TEXT},
+        {'type': T_EOF}
+    ]
+    assert_token_list_equal(tokens, expected)
+
+    text = '''{ par1 } Some free text (wow, [snafu])
+        {par2} (hint) Other text
+        {par3} (hint) ["foo"] Yet another doctext
+    '''
+    tokens = tokenize(text)
+    expected = [
+        {'type': T_LBRACE},
+        {'type': T_IDENTIFIER, 'value': 'par1'},
+        {'type': T_RBRACE},
+        {'type': T_TEXT},
+        {'type': T_LBRACE},
+        {'type': T_IDENTIFIER, 'value': 'par2'},
+        {'type': T_RBRACE},
+        {'type': T_LPAREN},
+        {'type': T_IDENTIFIER, 'value': 'hint'},
+        {'type': T_RPAREN},
+        {'type': T_TEXT},
+        {'type': T_LBRACE},
+        {'type': T_IDENTIFIER, 'value': 'par3'},
+        {'type': T_RBRACE},
+        {'type': T_LPAREN},
+        {'type': T_IDENTIFIER, 'value': 'hint'},
+        {'type': T_RPAREN},
+        {'type': T_LBRACK},
+        {'type': T_STRING, 'value': 'foo'},
+        {'type': T_RBRACK},
+        {'type': T_TEXT},
+        {'type': T_EOF}
+    ]
+    assert_token_list_equal(tokens, expected)
+
+
+def test_code_block():
+    text = '''
 ```
 import foo, bar
 params['foo'] = '{bar}'
 ```'''
-        tokens = [t for t in Lexer(text)]
-        expected = [
-            {'type': T_TEXT},
-            {'type': T_CODEMARK},
-            {'type': T_TEXT},
-            {'type': T_CODEMARK},
-            {'type': T_EOF}
-        ]
-        self.assertTokenListEqual(tokens, expected)
+    tokens = tokenize(text)
+    expected = [
+        {'type': T_TEXT},
+        {'type': T_CODEMARK},
+        {'type': T_TEXT},
+        {'type': T_CODEMARK},
+        {'type': T_EOF}
+    ]
+    assert_token_list_equal(tokens, expected)
 
-        text = '''{par1} Some text
+    text = '''{par1} Some text
 ```
 import foo, bar
 params['foo'] = '{bar}'
 ```
-    {par2} Other text.
-        '''
-        tokens = [t for t in Lexer(text)]
-        expected = [
-            {'type': T_LBRACE},
-            {'type': T_IDENTIFIER, 'value': 'par1'},
-            {'type': T_RBRACE},
-            {'type': T_TEXT},
-            {'type': T_CODEMARK},
-            {'type': T_TEXT},
-            {'type': T_CODEMARK},
-            {'type': T_TEXT},
-            {'type': T_LBRACE},
-            {'type': T_IDENTIFIER, 'value': 'par2'},
-            {'type': T_RBRACE},
-            {'type': T_TEXT},
-            {'type': T_EOF}
-        ]
-        self.assertTokenListEqual(tokens, expected)
+{par2} Other text.
+    '''
+    tokens = tokenize(text)
+    expected = [
+        {'type': T_LBRACE},
+        {'type': T_IDENTIFIER, 'value': 'par1'},
+        {'type': T_RBRACE},
+        {'type': T_TEXT},
+        {'type': T_CODEMARK},
+        {'type': T_TEXT},
+        {'type': T_CODEMARK},
+        {'type': T_TEXT},
+        {'type': T_LBRACE},
+        {'type': T_IDENTIFIER, 'value': 'par2'},
+        {'type': T_RBRACE},
+        {'type': T_TEXT},
+        {'type': T_EOF}
+    ]
+    assert_token_list_equal(tokens, expected)
 
-    def testComplexCodeBlock(self):
-        text = r'''```
+
+def test_complex_code_block():
+    text = r'''```
 foo = "bar\"baz"
 params['foo'] = "{bar}"
 bar = 'a\'b\'c'
@@ -269,11 +271,11 @@ baz = """
 codemarks in strings should be skipped !
 """
 ```'''
-        tokens = [t for t in Lexer(text)]
-        expected = [
-            {'type': T_CODEMARK},
-            {'type': T_TEXT},
-            {'type': T_CODEMARK},
-            {'type': T_EOF}
-        ]
-        self.assertTokenListEqual(tokens, expected)
+    tokens = tokenize(text)
+    expected = [
+        {'type': T_CODEMARK},
+        {'type': T_TEXT},
+        {'type': T_CODEMARK},
+        {'type': T_EOF}
+    ]
+    assert_token_list_equal(tokens, expected)
