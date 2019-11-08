a303812a432327fe9d726cbbcebfe6a6fb6bba00
ju1ius <jules.bernable@gmail.com>
2019-11-08 09:37:45 +0100

    [chore] splits diff module

--- clisnips/diff/myers.py
+++ clisnips/diff/myers.py
@@ -1,27 +1,23 @@
-"""
-Diff code extracted from meld
-(©) 2009 Piotr Piastucki <the_leech@users.berlios.de>
-License: GPLv2
-"""
-
 from collections import namedtuple
 from difflib import SequenceMatcher
+
+DiffChunk = namedtuple('DiffChunk', 'tag, start_a, end_a, start_b, end_b')
 
 
 def find_common_prefix(a, b):
     if not a or not b:
         return 0
     if a[0] == b[0]:
-        pointermax = min(len(a), len(b))
-        pointermid = pointermax
-        pointermin = 0
-        while pointermin < pointermid:
-            if a[pointermin:pointermid] == b[pointermin:pointermid]:
-                pointermin = pointermid
+        pointer_max = min(len(a), len(b))
+        pointer_mid = pointer_max
+        pointer_min = 0
+        while pointer_min < pointer_mid:
+            if a[pointer_min:pointer_mid] == b[pointer_min:pointer_mid]:
+                pointer_min = pointer_mid
             else:
-                pointermax = pointermid
-            pointermid = int((pointermax - pointermin) // 2 + pointermin)
-        return pointermid
+                pointer_max = pointer_mid
+            pointer_mid = int((pointer_max - pointer_min) // 2 + pointer_min)
+        return pointer_mid
     return 0
 
 
@@ -29,42 +25,39 @@ def find_common_suffix(a, b):
     if not a or not b:
         return 0
     if a[-1] == b[-1]:
-        pointermax = min(len(a), len(b))
-        pointermid = pointermax
-        pointermin = 0
-        while pointermin < pointermid:
-            a_tail = a[-pointermid:len(a) - pointermin]
-            b_tail = b[-pointermid:len(b) - pointermin]
+        pointer_max = min(len(a), len(b))
+        pointer_mid = pointer_max
+        pointer_min = 0
+        while pointer_min < pointer_mid:
+            a_tail = a[-pointer_mid:len(a) - pointer_min]
+            b_tail = b[-pointer_mid:len(b) - pointer_min]
             if a_tail == b_tail:
-                pointermin = pointermid
+                pointer_min = pointer_mid
             else:
-                pointermax = pointermid
-            pointermid = int((pointermax - pointermin) // 2 + pointermin)
-        return pointermid
+                pointer_max = pointer_mid
+            pointer_mid = int((pointer_max - pointer_min) // 2 + pointer_min)
+        return pointer_mid
     return 0
-
-
-DiffChunk = namedtuple('DiffChunk', 'tag, start_a, end_a, start_b, end_b')
 
 
 class MyersSequenceMatcher(SequenceMatcher):
 
-    def __init__(self, isjunk=None, a="", b=""):
+    def __init__(self, isjunk=None, a='', b=''):
         if isjunk is not None:
-            raise NotImplementedError('isjunk is not supported yet')
+            raise NotImplementedError('is_junk is not supported yet')
         super().__init__()
         self.a = a
         self.b = b
         self.matching_blocks = self.opcodes = None
-        self.aindex = []
-        self.bindex = []
+        self.a_index = []
+        self.b_index = []
         self.common_prefix = self.common_suffix = 0
         self.lines_discarded = False
 
     def set_seq2(self, b):
-        # we need to override since we don't support isjunk
+        # we need to override since we don't support is_junk
         # and the original methods call self.__chain_b()
-        # which uses self.isjunk 
+        # which uses self.is_junk
         if b is self.b:
             return
         self.b = b
@@ -81,7 +74,7 @@ class MyersSequenceMatcher(SequenceMatcher):
         return [DiffChunk._make(chunk) for chunk in opcodes]
 
     def get_difference_opcodes(self):
-        return [chunk for chunk in self.get_opcodes() if chunk.tag != "equal"]
+        return [chunk for chunk in self.get_opcodes() if chunk.tag != 'equal']
 
     def preprocess_remove_prefix_suffix(self, a, b):
         # remove common prefix and common suffix
@@ -96,14 +89,14 @@ class MyersSequenceMatcher(SequenceMatcher):
             if self.common_suffix > 0:
                 a = a[:len(a) - self.common_suffix]
                 b = b[:len(b) - self.common_suffix]
-        return (a, b)
+        return a, b
 
     def preprocess_discard_nonmatching_lines(self, a, b):
         # discard lines that do not match any line from the other file
         if len(a) == 0 or len(b) == 0:
-            self.aindex = []
-            self.bindex = []
-            return (a, b)
+            self.a_index = []
+            self.b_index = []
+            return a, b
 
         def index_matching(a, b):
             aset = frozenset(a)
@@ -114,8 +107,8 @@ class MyersSequenceMatcher(SequenceMatcher):
                     index.append(i)
             return matches, index
 
-        indexed_b, self.bindex = index_matching(a, b)
-        indexed_a, self.aindex = index_matching(b, a)
+        indexed_b, self.b_index = index_matching(a, b)
+        indexed_a, self.a_index = index_matching(b, a)
 
         # We only use the optimised result if it's worthwhile. The constant
         # represents a heuristic of how many lines constitute 'worthwhile'.
@@ -124,7 +117,7 @@ class MyersSequenceMatcher(SequenceMatcher):
         if self.lines_discarded:
             a = indexed_a
             b = indexed_b
-        return (a, b)
+        return a, b
 
     def preprocess(self):
         """
@@ -163,7 +156,7 @@ class MyersSequenceMatcher(SequenceMatcher):
         mb.reverse()
         self.matching_blocks = mb
 
-    def build_matching_blocks(self, lastsnake):
+    def build_matching_blocks(self, last_snake):
         """Build list of matching blocks based on snakes
 
         The resulting blocks take into consideration multiple preprocessing
@@ -176,44 +169,46 @@ class MyersSequenceMatcher(SequenceMatcher):
 
         common_prefix = self.common_prefix
         common_suffix = self.common_suffix
-        aindex = self.aindex
-        bindex = self.bindex
-        while lastsnake is not None:
-            lastsnake, x, y, snake = lastsnake
+        a_index = self.a_index
+        b_index = self.b_index
+        while last_snake is not None:
+            last_snake, x, y, snake = last_snake
             if self.lines_discarded:
                 # split snakes if needed because of discarded lines
                 x += snake - 1
                 y += snake - 1
-                xprev = aindex[x] + common_prefix
-                yprev = bindex[y] + common_prefix
+                x_prev = a_index[x] + common_prefix
+                y_prev = b_index[y] + common_prefix
                 if snake > 1:
-                    newsnake = 1
+                    new_snake = 1
                     for i in range(1, snake):
                         x -= 1
                         y -= 1
-                        xnext = aindex[x] + common_prefix
-                        ynext = bindex[y] + common_prefix
-                        if (xprev - xnext != 1) or (yprev - ynext != 1):
-                            matching_blocks.insert(0, (xprev, yprev, newsnake))
-                            newsnake = 0
-                        xprev = xnext
-                        yprev = ynext
-                        newsnake += 1
-                    matching_blocks.insert(0, (xprev, yprev, newsnake))
+                        x_next = a_index[x] + common_prefix
+                        y_next = b_index[y] + common_prefix
+                        if (x_prev - x_next != 1) or (y_prev - y_next != 1):
+                            matching_blocks.insert(0, (x_prev, y_prev, new_snake))
+                            new_snake = 0
+                        x_prev = x_next
+                        y_prev = y_next
+                        new_snake += 1
+                    matching_blocks.insert(0, (x_prev, y_prev, new_snake))
                 else:
-                    matching_blocks.insert(0, (xprev, yprev, snake))
+                    matching_blocks.insert(0, (x_prev, y_prev, snake))
             else:
                 matching_blocks.insert(0, (x + common_prefix,
                                            y + common_prefix, snake))
         if common_prefix:
             matching_blocks.insert(0, (0, 0, common_prefix))
         if common_suffix:
-            matching_blocks.append((len(self.a) - common_suffix,
-                                    len(self.b) - common_suffix,
-                                    common_suffix))
+            matching_blocks.append((
+                len(self.a) - common_suffix,
+                len(self.b) - common_suffix,
+                common_suffix
+            ))
         matching_blocks.append((len(self.a), len(self.b), 0))
         # clean-up to free memory
-        self.aindex = self.bindex = None
+        self.a_index = self.b_index = None
 
     def initialise(self):
         """
@@ -222,15 +217,14 @@ class MyersSequenceMatcher(SequenceMatcher):
         ("An O(NP) Sequence Comparison Algorithm", 1989)
         http://research.janelia.org/myers/Papers/np_diff.pdf
         """
-
         a, b = self.preprocess()
         m = len(a)
         n = len(b)
         middle = m + 1
-        lastsnake = None
+        last_snake = None
         delta = n - m + middle
-        dmin = min(middle, delta)
-        dmax = max(middle, delta)
+        d_min = min(middle, delta)
+        d_max = max(middle, delta)
         if n > 0 and m > 0:
             size = n + m + 2
             fp = [(-1, None)] * size
@@ -242,7 +236,7 @@ class MyersSequenceMatcher(SequenceMatcher):
                 # move along vertical edge
                 yv = -1
                 node = None
-                for km in range(dmin - p, delta, 1):
+                for km in range(d_min - p, delta, 1):
                     t = fp[km + 1]
                     if yv < t[0]:
                         yv, node = t
@@ -262,7 +256,7 @@ class MyersSequenceMatcher(SequenceMatcher):
                 # move along horizontal edge
                 yh = -1
                 node = None
-                for km in range(dmax + p, delta, -1):
+                for km in range(d_max + p, delta, -1):
                     t = fp[km - 1]
                     if yh <= t[0]:
                         yh, node = t
@@ -296,121 +290,8 @@ class MyersSequenceMatcher(SequenceMatcher):
                     node = (node, x - snake, y - snake, snake)
                 fp[delta] = (y, node)
                 if y >= n:
-                    lastsnake = node
+                    last_snake = node
                     break
-        self.build_matching_blocks(lastsnake)
+        self.build_matching_blocks(last_snake)
         self.postprocess()
         yield 1
-
-
-class InlineMyersSequenceMatcher(MyersSequenceMatcher):
-
-    def preprocess_discard_nonmatching_lines(self, a, b):
-
-        if len(a) <= 2 and len(b) <= 2:
-            self.aindex = []
-            self.bindex = []
-            return (a, b)
-
-        def index_matching_kmers(a, b):
-            aset = set([a[i:i + 3] for i in range(len(a) - 2)])
-            matches, index = [], []
-            next_poss_match = 0
-            # Start from where we can get a valid triple
-            for i in range(2, len(b)):
-                if b[i - 2:i + 1] not in aset:
-                    continue
-                # Make sure we don't re-record matches from overlapping kmers
-                for j in range(max(next_poss_match, i - 2), i + 1):
-                    matches.append(b[j])
-                    index.append(j)
-                next_poss_match = i + 1
-            return matches, index
-
-        indexed_b, self.bindex = index_matching_kmers(a, b)
-        indexed_a, self.aindex = index_matching_kmers(b, a)
-
-        # We only use the optimised result if it's worthwhile. The constant
-        # represents a heuristic of how many lines constitute 'worthwhile'.
-        self.lines_discarded = (len(b) - len(indexed_b) > 10 or
-                                len(a) - len(indexed_a) > 10)
-        if self.lines_discarded:
-            a = indexed_a
-            b = indexed_b
-        return (a, b)
-
-
-class SyncPointMyersSequenceMatcher(MyersSequenceMatcher):
-
-    def __init__(self, isjunk=None, a="", b="", syncpoints=None):
-        super().__init__(isjunk, a, b)
-        self.isjunk = isjunk
-        self.syncpoints = syncpoints
-
-    def initialise(self):
-        if self.syncpoints is None or len(self.syncpoints) == 0:
-            for i in MyersSequenceMatcher.initialise(self):
-                yield i
-        else:
-            chunks = []
-            ai = 0
-            bi = 0
-            for aj, bj in self.syncpoints:
-                chunks.append((ai, bi, self.a[ai:aj], self.b[bi:bj]))
-                ai = aj
-                bi = bj
-            if ai < len(self.a) or bi < len(self.b):
-                chunks.append((ai, bi, self.a[ai:], self.b[bi:]))
-
-            self.split_matching_blocks = []
-            self.matching_blocks = []
-            for ai, bi, a, b in chunks:
-                matching_blocks = []
-                matcher = MyersSequenceMatcher(self.isjunk, a, b)
-                for i in matcher.initialise():
-                    yield None
-                blocks = matcher.get_matching_blocks()
-                l = len(matching_blocks) - 1
-                if l >= 0 and len(blocks) > 1:
-                    aj = matching_blocks[l][0]
-                    bj = matching_blocks[l][1]
-                    bl = matching_blocks[l][2]
-                    if (aj + bl == ai and bj + bl == bi and
-                        blocks[0][0] == 0 and blocks[0][1] == 0):
-                        block = blocks.pop(0)
-                        matching_blocks[l] = (aj, bj, bl + block[2])
-                for x, y, l in blocks[:-1]:
-                    matching_blocks.append((ai + x, bi + y, l))
-                self.matching_blocks.extend(matching_blocks)
-                # Split matching blocks each need to be terminated to get our
-                # split chunks correctly created
-                self.split_matching_blocks.append(
-                    matching_blocks + [(ai + len(a), bi + len(b), 0)])
-            self.matching_blocks.append((len(self.a), len(self.b), 0))
-            yield 1
-
-    def get_opcodes(self):
-        # This is just difflib.SequenceMatcher.get_opcodes in which we instead
-        # iterate over our internal set of split matching blocks.
-        if self.opcodes is not None:
-            return self.opcodes
-        i = j = 0
-        self.opcodes = opcodes = []
-        self.get_matching_blocks()
-        for matching_blocks in self.split_matching_blocks:
-            for ai, bj, size in matching_blocks:
-                tag = ''
-                if i < ai and j < bj:
-                    tag = 'replace'
-                elif i < ai:
-                    tag = 'delete'
-                elif j < bj:
-                    tag = 'insert'
-                if tag:
-                    opcodes.append((tag, i, ai, j, bj))
-                i, j = ai + size, bj + size
-                # the list of matching blocks is terminated by a
-                # sentinel with size 0
-                if size:
-                    opcodes.append(('equal', ai, i, bj, j))
-        return [DiffChunk._make(chunk) for chunk in opcodes]
