b5e4e87c54c662c65cc5ad0b79d0bcc66051e148
ju1ius <jules.bernable@gmail.com>
2023-11-20 08:35:55 +0100

    feat: adds gzip compression to dump command

--- clisnips/cli/dump_command.py
+++ clisnips/cli/dump_command.py
@@ -1,7 +1,8 @@
 import argparse
 import logging
-import sys
+from pathlib import Path
 import time
+from typing import TextIO
 
 from .command import Command
 
@@ -12,15 +13,26 @@ class DumpCommand(Command):
     @classmethod
     def configure(cls, action: argparse._SubParsersAction):
         cmd = action.add_parser('dump', help='Runs a SQL dump of the database.')
-        cmd.add_argument('file', type=argparse.FileType('w'), default=sys.stdout)
+        cmd.add_argument('-c', '--compress', action='store_true', help='Compresses the output using gzip.')
+        cmd.add_argument('file', type=Path)
 
     def run(self, argv) -> int:
         start_time = time.time()
+        logger.info(f'Dumping database to {argv.file}')
 
-        db = self.container.database
-        for line in db.connection.iterdump():
-            argv.file.write(f'{line}\n')
+        if argv.compress:
+            import gzip
+            with gzip.open(argv.file, 'wt') as fp:
+                self._dump(fp) # type: ignore (the `wt` mode implies TextIO)
+        else:
+            with open(argv.file, 'w') as fp:
+                self._dump(fp)
 
         elapsed = time.time() - start_time
-        logger.info(f'Dumped database to {argv.file.name} in {elapsed:.1f} seconds.', extra={'color': 'success'})
+        logger.info(f'Done in {elapsed:.1f} seconds.', extra={'color': 'success'})
         return 0
+
+    def _dump(self, fp: TextIO):
+        db = self.container.database
+        for line in db.connection.iterdump():
+            fp.write(f'{line}\n')
