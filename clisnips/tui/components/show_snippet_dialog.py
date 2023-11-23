de1f145fc21bbf0c6929718c9c02b9d46c2883e3
ju1ius <jules.bernable@gmail.com>
2023-11-23 08:47:02 +0100

    feat: displays additional info in show snippet dialog

--- clisnips/tui/components/show_snippet_dialog.py
+++ clisnips/tui/components/show_snippet_dialog.py
@@ -1,5 +1,8 @@
+import time
+
 import urwid
 
+from clisnips.database import Snippet
 from clisnips.tui.highlighters import highlight_command, highlight_documentation
 from clisnips.tui.urwid_types import TextMarkup
 from clisnips.tui.widgets.dialog import Dialog
@@ -8,19 +11,19 @@ from clisnips.tui.widgets.divider import HorizontalDivider
 
 def _create_field(label: TextMarkup, content: TextMarkup):
     field = urwid.Pile(
-        [
+        (
             urwid.Text(label),
             urwid.Text(content),
-        ]
+        ),
     )
     return field
 
 
 class ShowSnippetDialog(Dialog):
-    def __init__(self, parent, snippet):
+    def __init__(self, parent, snippet: Snippet):
         body = urwid.ListBox(
             urwid.SimpleListWalker(
-                [
+                (
                     _create_field('Title', snippet['title']),
                     HorizontalDivider(),
                     _create_field('Tags', snippet['tag']),
@@ -28,9 +31,17 @@ class ShowSnippetDialog(Dialog):
                     _create_field('Command', highlight_command(snippet['cmd'])),
                     HorizontalDivider(),
                     _create_field('Documentation', highlight_documentation(snippet['doc'])),
-                    # HorizontalDivider(),
-                ]
-            )
+                    HorizontalDivider(),
+                    urwid.Text(f"Created on: {date(snippet['created_at'])}"),
+                    urwid.Text(f"Last used on: {date(snippet['last_used_at'])}"),
+                    urwid.Text(f"Usage count: {snippet['usage_count']}"),
+                    urwid.Text(f"Ranking: {snippet['ranking']}"),
+                ),
+            ),
         )
 
         super().__init__(parent, body)
+
+
+def date(timestamp: float):
+    return time.strftime('%Y-%m-%d %H:%M:%S UTC', time.gmtime(timestamp))
